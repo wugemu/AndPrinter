0b0110672c31217b0865ae991e3dfbeb34cdefc9
jessie <245868553@qq.com>
2018-06-15 11:35:37 +0800

    代码上传

---
+++ GPprinter-master/app/src/main/java/com/example/sam/gpsample/utils/QRCodePicUtil.java
@@ -0,0 +1,160 @@
+package com.example.sam.gpsample.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.PointF;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by root on 18-6-6.
+ */
+
+public class QRCodePicUtil {
+    public static Bitmap createQRImage(Activity context, String url, int QR_WIDTH) {
+        try {
+            //判断URL合法性
+            if (url == null || "".equals(url) || url.length() < 1) {
+                return null;
+            }
+            //配置参数
+            Map hints = new HashMap<>();
+            hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
+            //容错级别
+            hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+            //设置空白边距的宽度
+            hints.put(EncodeHintType.MARGIN, 1); //default is 4
+            //设置空白边距的宽度
+
+            //图像数据转换，使用了矩阵转换
+            BitMatrix bitMatrix = new QRCodeWriter().encode(url, BarcodeFormat.QR_CODE, QR_WIDTH, QR_WIDTH, hints);
+
+            int[] pixels = new int[QR_WIDTH * QR_WIDTH];
+            //下面这里按照二维码的算法，逐个生成二维码的图片，
+            //两个for循环是图片横列扫描的结果
+            for (int y = 0; y < QR_WIDTH; y++) {
+                for (int x = 0; x < QR_WIDTH; x++) {
+                    if (bitMatrix.get(x, y)) {
+                        pixels[y * QR_WIDTH + x] = 0xff000000;
+                    } else {
+                        pixels[y * QR_WIDTH + x] = 0xffffffff;
+                    }
+                }
+            }
+            //生成二维码图片的格式，使用ARGB_8888
+            Bitmap bitmap = Bitmap.createBitmap(QR_WIDTH, QR_WIDTH, Bitmap.Config.ARGB_8888);
+            bitmap.setPixels(pixels, 0, QR_WIDTH, 0, 0, QR_WIDTH, QR_WIDTH);
+            return bitmap;
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static Bitmap creatBarcode(Activity context, String contents,
+                                     int desiredWidth, int desiredHeight, boolean displayCode) {
+        Bitmap ruseltBitmap = null;
+        int marginW = 20;
+        BarcodeFormat barcodeFormat = BarcodeFormat.CODE_128;
+
+        if (displayCode) {
+            Bitmap barcodeBitmap = encodeAsBitmap(contents, barcodeFormat,
+                    desiredWidth, desiredHeight);
+            Bitmap codeBitmap = creatCodeBitmap(contents, desiredWidth + 2
+                    * marginW, desiredHeight, context);
+            ruseltBitmap = mixtureBitmap(barcodeBitmap, codeBitmap, new PointF(
+                    0, desiredHeight));
+        } else {
+            ruseltBitmap = encodeAsBitmap(contents, barcodeFormat,
+                    desiredWidth, desiredHeight);
+        }
+
+        return ruseltBitmap;
+    }
+
+    protected static Bitmap encodeAsBitmap(String contents,
+                                           BarcodeFormat format, int desiredWidth, int desiredHeight) {
+        final int WHITE = 0xFFFFFFFF;
+        final int BLACK = 0xFF000000;
+
+        MultiFormatWriter writer = new MultiFormatWriter();
+        BitMatrix result = null;
+        try {
+            result = writer.encode(contents, format, desiredWidth,
+                    desiredHeight, null);
+        } catch (WriterException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        int width = result.getWidth();
+        int height = result.getHeight();
+        int[] pixels = new int[width * height];
+        // All are 0, or black, by default
+        for (int y = 0; y < height; y++) {
+            int offset = y * width;
+            for (int x = 0; x < width; x++) {
+                pixels[offset + x] = result.get(x, y) ? BLACK : WHITE;
+            }
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(width, height,
+                Bitmap.Config.ARGB_8888);
+        bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+        return bitmap;
+    }
+
+    protected static Bitmap creatCodeBitmap(String contents, int width,
+                                            int height, Context context) {
+        TextView tv = new TextView(context);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        tv.setLayoutParams(layoutParams);
+        tv.setText(contents);
+        tv.setHeight(height);
+        tv.setGravity(Gravity.CENTER_HORIZONTAL);
+        tv.setWidth(width);
+        tv.setDrawingCacheEnabled(true);
+        tv.setTextColor(Color.BLACK);
+        tv.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
+                View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+        tv.layout(0, 0, tv.getMeasuredWidth(), tv.getMeasuredHeight());
+
+        tv.buildDrawingCache();
+        Bitmap bitmapCode = tv.getDrawingCache();
+        return bitmapCode;
+    }
+    protected static Bitmap mixtureBitmap(Bitmap first, Bitmap second,
+                                          PointF fromPoint) {
+        if (first == null || second == null || fromPoint == null) {
+            return null;
+        }
+        int marginW = 20;
+        Bitmap newBitmap = Bitmap.createBitmap(
+                first.getWidth() + second.getWidth() + marginW,
+                first.getHeight() + second.getHeight(), Bitmap.Config.ARGB_4444);
+        Canvas cv = new Canvas(newBitmap);
+        cv.drawBitmap(first, marginW, 0, null);
+        cv.drawBitmap(second, fromPoint.x, fromPoint.y, null);
+        cv.save(Canvas.ALL_SAVE_FLAG);
+        cv.restore();
+
+        return newBitmap;
+    }
+}
